f733452aa05a785cc86ad32ecd3d44694e181ccb
JOHN-KUN1 <gofwenritji@gmail.com>
2026-03-21 21:45:11 +0100

    add file picker to retrieve file from local storage

--- lib/screens/ai_analysis_screen.dart
+++ lib/screens/ai_analysis_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:open_file_manager/open_file_manager.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AiAnalysisScreen extends StatefulWidget {
   const AiAnalysisScreen({super.key});
@@ -14,7 +19,7 @@ class _AiAnalysisScreenState extends State<AiAnalysisScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'AI Anaylysis',
+          'AI Analysis',
           style: GoogleFonts.poppins(
             color: Colors.black,
             fontWeight: FontWeight.bold,
@@ -27,11 +32,42 @@ class _AiAnalysisScreenState extends State<AiAnalysisScreen> {
           mainAxisAlignment: .center,
           children: [
             IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.inbox_outlined,
+              onPressed: () async {
+                if (await Permission
+                    .manageExternalStorage
+                    .isPermanentlyDenied || await Permission.storage.isPermanentlyDenied) {
+                  if (!mounted) return;
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text(
+                          'Permission Request',
+                          style: GoogleFonts.poppins(),
+                        ), 
+                        content: Text('Storage permission is required to perform Ai analysis', style: GoogleFonts.poppins(),),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              openAppSettings();
+                            },
+                            child: Text(
+                              'Open settings',
+                              style: GoogleFonts.poppins(),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }else if(await Permission.manageExternalStorage.request().isGranted || await Permission.storage.request().isGranted){
+                  FilePickerResult? result = await FilePicker.platform.pickFiles();
+                }
+              },
+              icon: const Icon(
+                Icons.file_upload_outlined,
                 size: 100,
-                color: Colors.grey,
+                color: Colors.blue,
               ),
             ),
             const SizedBox(
